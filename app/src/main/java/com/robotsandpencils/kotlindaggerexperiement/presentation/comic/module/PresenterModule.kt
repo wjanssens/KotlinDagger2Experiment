826e55bffbb93d38929d0e0f9c1eed332676f228
Neal Sanche <neal.sanche@robotsandpencils.com>
2017-09-27 14:41:21 -0600

    Use lifecycle aware thread queues everywhere

--- app/src/main/java/com/robotsandpencils/kotlindaggerexperiement/presentation/comic/module/PresenterModule.kt
+++ app/src/main/java/com/robotsandpencils/kotlindaggerexperiement/presentation/comic/module/PresenterModule.kt
@@ -1,7 +1,8 @@
 package com.robotsandpencils.kotlindaggerexperiement.presentation.comic.module
 
 import com.robotsandpencils.kotlindaggerexperiement.app.repositories.XkcdRepository
-import com.robotsandpencils.kotlindaggerexperiement.presentation.base.UiThreadQueue
+import com.robotsandpencils.kotlindaggerexperiement.presentation.base.LifecycleAwareUiThreadQueue
+import com.robotsandpencils.kotlindaggerexperiement.presentation.comic.ComicFragment
 import com.robotsandpencils.kotlindaggerexperiement.presentation.comic.Contract
 import com.robotsandpencils.kotlindaggerexperiement.presentation.comic.Presenter
 import dagger.Module
@@ -15,8 +16,8 @@ internal abstract class PresenterModule {
     companion object {
         @Provides
         @Scope
-        @JvmStatic internal fun providesPresenter(repository: XkcdRepository): Contract.Presenter {
-            return Presenter(repository, UiThreadQueue())
+        @JvmStatic internal fun providesPresenter(fragment: ComicFragment, repository: XkcdRepository): Contract.Presenter {
+            return Presenter(repository, LifecycleAwareUiThreadQueue(fragment))
         }
     }
 }
--- app/src/main/java/com/robotsandpencils/kotlindaggerexperiement/presentation/counter/module/PresenterModule.kt
+++ app/src/main/java/com/robotsandpencils/kotlindaggerexperiement/presentation/counter/module/PresenterModule.kt
@@ -1,8 +1,9 @@
 package com.robotsandpencils.kotlindaggerexperiement.presentation.counter.module
 
 import com.robotsandpencils.kotlindaggerexperiement.app.repositories.MainRepository
-import com.robotsandpencils.kotlindaggerexperiement.presentation.base.UiThreadQueue
+import com.robotsandpencils.kotlindaggerexperiement.presentation.base.LifecycleAwareUiThreadQueue
 import com.robotsandpencils.kotlindaggerexperiement.presentation.counter.Contract
+import com.robotsandpencils.kotlindaggerexperiement.presentation.counter.CounterFragment
 import com.robotsandpencils.kotlindaggerexperiement.presentation.counter.Presenter
 import dagger.Module
 import dagger.Provides
@@ -15,8 +16,8 @@ internal abstract class PresenterModule {
     companion object {
         @Provides
         @Scope
-        @JvmStatic internal fun providesPresenter(mainRepository: MainRepository): Contract.Presenter {
-            return Presenter(mainRepository, UiThreadQueue())
+        @JvmStatic internal fun providesPresenter(fragment: CounterFragment, mainRepository: MainRepository): Contract.Presenter {
+            return Presenter(mainRepository, LifecycleAwareUiThreadQueue(fragment))
         }
     }
 }
